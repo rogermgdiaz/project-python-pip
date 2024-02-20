fd095137bcaaea54900ac8d9c5928d93e2bbf54b
rogermgdiaz <rogermgdiaz@gmail.com>
2024-02-20 12:49:28 -0500

    changing main.py and adding new requirements to requirements.txt

--- app/main.py
+++ app/main.py
@@ -1,4 +1,19 @@
 import store 
+from fastapi import FastAPI
+
+app = FastAPI()
+
+@app.get('/')
+def get_list():
+    return[1,2,3]
+
+@app.get('/contact')
+def get_list():
+   name = input('Tu nombre: ')
+   return(f"Hola {name}")
+
+
+
 
 def run():
     store.get_categories()
