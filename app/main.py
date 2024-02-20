e0cf8d4737e6e1c029f86f8ab68c92ae63b760ea
rogermgdiaz <rogermgdiaz@gmail.com>
2024-02-20 12:00:15 -0500

    Adding new stuff: using pandas in charts and implementing new stuff in web server

--- app/main.py
+++ app/main.py
@@ -1,1 +1,7 @@
-print('Hello World')
+import store 
+
+def run():
+    store.get_categories()
+    
+if __name__ == '__main__':
+    run()
--- charts/app/main.py
+++ charts/app/main.py
@@ -1,10 +1,29 @@
 import utils
 import read_csv
 import charts
+import pandas as pd
+
 
 def run():
-  data = list(read_csv.read_csv('data.csv'))
+
+
+
   
+  # data = list(filter(lambda item: item['Continent'] == 'South America', data))
+
+  # countries = list(map(lambda x: x['Country'], data))
+  # percentages = list(map(lambda x: x['World Population Percentage'], data))
+  # charts.generate_pie_chart(countries, percentages)
+  
+  
+  df= pd.read_csv('data.csv')
+  df=df[df['Continent']=='Africa']
+  
+  countries = df['Country'].values
+  percentages= df['World Population Percentage'].values
+  charts.generate_pie_chart(countries, percentages)
+  
+  data = list(read_csv.read_csv('data.csv'))
   country = input('Type Country => ').title()
   print(country)
 
@@ -16,12 +35,7 @@ def run():
     labels, values = utils.get_population(country_data)
     charts.generate_bar_chart(country_data['Country'], labels, values)
 
-  data = list(filter(lambda item: item['Continent'] == 'South America', data))
 
-  countries = list(map(lambda x: x['Country'], data))
-  percentages = list(map(lambda x: x['World Population Percentage'], data))
-  charts.generate_pie_chart(countries, percentages)
-  
 
 if __name__ == '__main__':
   run()
